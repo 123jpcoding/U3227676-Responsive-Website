5d9a23d8b4ef652ff1e6873c795bb241d037e023
123jpcoding <jcphelps02@gmail.com>
2023-08-29 14:18:10 +1000

    Revert "Judo Website"
    This reverts commit 33129d5f6271852baa46f85b6cf8f9a1cfc2a7cd.

--- file.js
+++ file.js
@@ -1,54 +1,56 @@
 //JavaScript for toggle menu//
-var navLinks = document.getElementById("navLinks");
-function showMenu() {
-    navLinks.style.right = "0";
-}
-function hideMenu() {
-    navLinks.style.right = "-200px";
-}
-
-//Announcements Slider//
-var slide = document.getElementById("slide");
-var upArrow = document.getElementById("upArrow");
-var downArrow = document.getElementById("downArrow");
-
-let x = 0;
-
-upArrow.onclick = function () {
-    if (x > "-900") {
-        x = x - 300;
-        slide.style.top = x + "px";
+    var navLinks = document.getElementById("navLinks");
+    function showMenu() {
+        navLinks.style.right = "0";
     }
-}
-downArrow.onclick = function () {
-    if (x < 0) {
-        x = x + 300;
-        slide.style.top = x + "px";
+    function hideMenu() {
+        navLinks.style.right = "-200px";
     }
+
+//Slideshow//
+const mySlider = document.querySelectorAll('.mySlider'),
+dot = document.querySelectorAll('.dot');
+
+let counter = 1;
+slidefun(counter);
+
+let timer = setInterval(autoslide, 8000);
+function autoSlide() {
+    counter += 1;
+    slidefun(counter);
 }
 
-//Calendar//
-$(document).ready(function () {
-    $('#calendar').evoCalendar({
+function plusSlides(n) {
+    counter += n;
+    slidefun(counter);
+    resetTimer();
+}
 
-        theme: "Midnight Blue",
-        calendarEvents: [{
-                id: 'event1', // Event's ID (required)
-                name: "Judo Tournament", // Event name (required)
-                date: "May/22/2022", // Event date (required)
-                description: "Olympia Judo Club is holding a Judo tournament from 9.30am at MacKinnon St, Wanniassa ACT 2903",
-                type: "event", // Event type (required)
-                everyYear: true, // Same event every year (optional)
-                color: "#7bc1df"
-            },
-            {
-                id: 'event2',
-                name: "Training Cancelled",
-                date: "May/05/2022", // Date range
-                description: "Judo training cancelled due to Marist school musical practice.", // Event description (optional)
-                type: "event",
-                color: "#7bc1df"
-            }
-        ]
-    });
-})
+function currentSlide(n) {
+    counter = n;
+    slidefun(counter);
+    resetTimer();
+}
+
+function resetTimer(){
+clearInterval(timer);
+timer = setInterval(autoSlide, 8000);
+}
+
+function slidefun(n) {
+    let i;
+    for(i = 0;i<mySlider.length;i++) {
+        myslide[i].style.display = "none";
+    }
+    for(i = 0;1<dot.length;i++) {
+        dot[i].classList.remove('active');
+    }
+    if(n > mySlider.length) {
+        counter = 1;
+    }
+    if(n < 1) {
+        counter = mySlide.length;
+    }
+    mySlider[counter - 1].style.display = "block";
+    dot[counter - 1].classList.add('active');
+}
